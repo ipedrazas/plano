ac290333301f45b169e3e0f60570233328fe27b3
Ivan Pedrazas <ipedrazas@gmail.com>
2015-04-16 12:50:05 +0100

    cleanup

--- python/settings.py
+++ python/settings.py
@@ -4,31 +4,8 @@ MONGO_HOST = 'mongodb'
 MONGO_PORT = 27017
 MONGO_DBNAME = 'plano01'
 
-# Enable reads (GET), inserts (POST) and DELETE for resources/collections
-# (if you omit this line, the API will default to ['GET'] and provide
-# read-only access to the endpoint).
 RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
-
-# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
-# individual items  (defaults to read-only item access).
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
-
-logs = {
-    'schema': {
-        'user': {
-            'type': 'string'
-        },
-        'source_id': {
-            'type': 'string'
-        },
-        'action': {
-            'type': 'string'
-        },
-        'value': {
-            'type': 'string'
-        }
-    }
-}
 
 applications = {
     'schema': {
@@ -55,7 +32,6 @@ runtimes = {
 }
 
 DOMAIN = {
-    'logs': logs,
     'applications': applications,
     'components': components,
     'runtimes': runtimes,
