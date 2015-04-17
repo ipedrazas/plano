25fb8f438834f400a2a29f812767bb9a2f15220d
Ivan Pedrazas <ipedrazas@gmail.com>
2015-04-17 11:49:58 +0100

    cors dependency added

--- python/settings.py
+++ python/settings.py
@@ -7,6 +7,14 @@ MONGO_DBNAME = 'plano01'
 RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
+# Enable CORS for all domains
+X_DOMAINS = "*"
+X_HEADERS = "Content-Type, Accept, Authorization, X-Requested-With, " \
+    " Access-Control-Request-Headers, Access-Control-Allow-Origin, " \
+    " Access-Control-Allow-Credentials, X-HTTP-Method-Override, mozSystem, " \
+    " Access-Control-Allow-Methods, If-Match "
+
+
 applications = {
     'schema': {
         'name': {
